da05c62cbace29ccd925de0b17ac5e72b3d97fd3
Saucter <alhosni1615@gmail.com>
2023-03-27 16:57:36 +0400

    Add grades dictionary

--- Shared/Entities/Students.cs
+++ Shared/Entities/Students.cs
@@ -9,13 +9,28 @@ namespace Dashboard_Project.Shared.Entities
 {
     public class Students
     {
+        public Students()
+        {
+            Grades = GradesInitilizor(Class);
+        }
         public int id {get; set;}
         public int CandidateNumber {get; set;}
         public string Name {get; set;}
         public DateTime Birthdate {get; set;}
         private string Username {get; set;}
         private string Password {get; set;} = "rgotc123";
-        public CLasses Class {get; set;}
+        public CLasses Class {get; set;} = new CLasses();
         public Dictionary<SubjectsList, Subjects> Grades {get; set;}
+
+        public Dictionary<SubjectsList, Subjects> GradesInitilizor(CLasses Class)
+        {
+            SubjectSelector Selector = new SubjectSelector();
+            Dictionary<SubjectsList, Subjects> grades = new Dictionary<SubjectsList, Subjects>();
+            foreach(var subject in Selector.Selector(Class))
+            {
+                grades.Add(subject, new Subjects());
+            }
+            return grades;
+        }
     }
 }
